b6e55948e32f14588c89bbeb92d1f07efa5921d0
Brandon Lim <brandonlim.lim@gmail.com>
2019-09-22 00:43:57 +1000

    Use single quotes for consistency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from minpair import __version__
 from setuptools import find_packages, setup
 import os
 
-with open("README.md") as readme_file:
+with open('README.md') as readme_file:
     long_description = readme_file.read()
 
 with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
@@ -13,23 +13,23 @@ with open(os.path.join(os.path.dirname(__file__), 'test_requirements.txt')) as f
     tests_require = f.read().splitlines()
 
 setup(
-    name="minpair",
+    name='minpair',
     version=__version__,
-    author="Brandon Lim",
-    author_email="brandonlim.lim@gmail.com",
-    description="Generate minimal pairs (and minimal sets).",
+    author='Brandon Lim',
+    author_email='brandonlim.lim@gmail.com',
+    description='Generate minimal pairs (and minimal sets).',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/brandonlim-hs/minpair",
+    long_description_content_type='text/markdown',
+    url='https://github.com/brandonlim-hs/minpair',
     packages=find_packages(),
-    license="MIT License",
+    license='MIT License',
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
     ],
     install_requires=install_requires,
-    use_scm_version={"version_scheme": "post-release",
-                     "write_to": "minpair/_version.py", },
-    setup_requires=["setuptools_scm"],
+    use_scm_version={'version_scheme': 'post-release',
+                     'write_to': 'minpair/_version.py', },
+    setup_requires=['setuptools_scm'],
 )
